d74aca55441e8b6a2f6f7a1d0d7a1201b911f3bd
miguel <miguel.silva294@etec.sp.gov.br>
2026-02-17 17:15:29 -0300

    footer_precisando_de_informaçoes

--- src/backend/php/pages/home.php
+++ src/backend/php/pages/home.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Icone do fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Icone do fontawesome -->
+
     <link rel="stylesheet" href="\soee\src\frontend\css\home.css">
     <link rel="icon" type="image/png" href="/soee/src/imgs/logo-soee.png">
     <title>Início - S.O.E.E</title>
@@ -36,13 +41,69 @@
         <main>
             <section class="section-main">
                 <h1> TESTE PAgina</h1>
+                <h2> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis facilis quia accusamus quibusdam, mollitia perferendis consequatur, doloremque dignissimos explicabo dolores fugiat asperiores voluptatum dolorem molestiae laborum odio veritatis voluptatibus excepturi.</h2>
             </section>
         </main>
-        <footer>
-            <section class="section-footer">
 
-            </section>
+
+
+        <footer>
+            <div id="footer_content">
+                <div id="footer_contacts">
+                    <h1>Etec Juscelino Kubitschek de Oliveira</h1>
+                    <p>Rede social</p>
+                    <div id="fotter_social_media">
+                        <a href="https://www.instagram.com/etecjko?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" class="footer-link" id="instagram">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <ul class="footer-list">
+                    <li>
+                        <h3>Comunicação</h3>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">(11) 4053-9400</a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Etec</a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Etec</a>
+                    </li>
+                </ul>
+                <ul class="footer-list">
+                    <li>
+                        <h3>alguma coisa2</h3>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Etec</a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="footer-link">Etec</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#" class="footer-link">Etec</a>
+                    </li>
+
+                </ul> 
+            
+
+            </div>
+            <div id="footer_copyright">
+&#169
+2026 todos os direitos reservados
+
+            </div>
         </footer>
+
+
+
         <script>
 
         </script>
